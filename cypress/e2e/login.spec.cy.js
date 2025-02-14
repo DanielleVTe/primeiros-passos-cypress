6432c1841c3334dcaf4dea50a1cc746d918d467d
Danielle Terciotti <dvterci@gmail.com>
2025-02-14 08:52:12 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,23 +1,29 @@
 describe('Orange HRM Tests', () => {
+  const selectorList = {
+    usernameField: "[name='username]",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert:"[role='alert']"
+    }
+
   it('login - success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    cy.get('input[name="username"]').type('Admin');
-    cy.get('input[name="password"]').type('admin123');
-    cy.get('.oxd-button').click();
+    cy.get (selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click();
     
     // Verifica se a URL é a esperada após o login
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index');
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
-
-  });
-  it('login - success', () => {
+    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+  })
+  it('login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    cy.get('input[name="username"]').type('test');
-    cy.get('input[name="password"]').type('test');
-    cy.get('.oxd-button').click();
-    cy.get("[role='alert']")
-    
+    cy.get(selectorList.usernameField).type('test');
+    cy.get(selectorList.passwordField).type('test');
+    cy.get(selectorList.loginButton).click();
+    cy.get(selectorList.wrongCredentialAlert)    
   });
 });
